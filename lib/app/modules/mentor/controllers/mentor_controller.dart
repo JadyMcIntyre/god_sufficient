1f3015bb46c5d55451b84d6b682eaac5a9da6879
JadyMcIntyre <mcintyrej@out.co.za>
2023-10-25 07:39:36 +0200

    testing changes controller

--- lib/app/modules/mentor/controllers/mentor_controller.dart
+++ lib/app/modules/mentor/controllers/mentor_controller.dart
@@ -1,9 +1,6 @@
 import 'package:get/get.dart';
 
 class MentorController extends GetxController {
-  //TODO: Implement MentorController
-
-  final count = 0.obs;
   @override
   void onInit() {
     super.onInit();
@@ -18,6 +15,4 @@ class MentorController extends GetxController {
   void onClose() {
     super.onClose();
   }
-
-  void increment() => count.value++;
 }
