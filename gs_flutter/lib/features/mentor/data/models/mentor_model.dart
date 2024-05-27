c0e68be0881a21a556e4546c733ccd4837ff08cf
JadyMcIntyre <116111920+JadyMcIntyre@users.noreply.github.com>
2024-05-27 14:27:38 +0200

    removed old entity used new

--- gs_flutter/lib/features/mentor/data/models/mentor_model.dart
+++ gs_flutter/lib/features/mentor/data/models/mentor_model.dart
@@ -1,11 +1,10 @@
 import 'package:god_sufficient/features/mentor/domain/entities/mentor_entity.dart';
-import 'package:god_sufficient/src/domain/entities/mentor_response_entity.dart';
 import 'package:json_annotation/json_annotation.dart';
 
 part 'mentor_model.g.dart';
 
 @JsonSerializable(createToJson: false)
-class MentorModel extends MentorResponseEntity {
+class MentorModel extends MentorEntity {
   MentorModel({required super.id, required super.name, required super.expertise, required super.description});
 
   factory MentorModel.fromJson(Map<String, dynamic> json) => _$MentorModelFromJson(json);
